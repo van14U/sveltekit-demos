92e762bbca9ebd6acda306c270bdedf25bb07b95
Ivan B <sensenabi@gmail.com>
2024-01-11 15:53:39 -0500

    test response headers

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,21 +1,27 @@
-import type { RequestHandler } from "./$types";
+import type { RequestHandler } from './$types';
 
 export const GET = (async (event) => {
   await new Promise((resolve) => setTimeout(resolve, 2000));
   const title = await fetch('https://hacker-news.firebaseio.com/v0/newstories.json')
-    .then(response => response.json())
-    .then(async storyIds => {
+    .then((response) => response.json())
+    .then(async (storyIds) => {
       const randomIdx = Math.floor(Math.random() * storyIds.length);
       const storyId = storyIds[randomIdx];
-      console.log({ storyId })
+      console.log({ storyId });
       return fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`)
-        .then(response => response.json())
-        .then(story => {
-          return (story.title) as string;
-        })
-    })
-  event.setHeaders({
-    'Cache-Control': 'public, max-age=60, s-maxage=60, stale-while-revalidate=86400, stale-if-error=86400',
-  })
-  return new Response(JSON.stringify({ title }))
+        .then((response) => response.json())
+        .then((story) => {
+          return story.title as string;
+        });
+    });
+  // event.setHeaders({
+  // 	'Cache-Control':
+  // 		'public, max-age=60, s-maxage=60, stale-while-revalidate=86400, stale-if-error=86400'
+  // });
+  return new Response(JSON.stringify({ title }), {
+    headers: {
+      'Cache-Control':
+        'public, max-age=0, s-maxage=60, stale-while-revalidate=86400, stale-if-error=86400'
+    }
+  });
 }) satisfies RequestHandler;
